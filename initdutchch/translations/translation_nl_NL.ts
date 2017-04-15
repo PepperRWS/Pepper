2fa9ed8ca83939c10ca8429ba72e42fd531b5cf4
RobertRWS <Robert@192.168.2.11>
2017-04-16 01:08:28 +0200

    NO CHANGE

--- initdutchch/translations/translation_nl_NL.ts
+++ initdutchch/translations/translation_nl_NL.ts
@@ -1,3 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl_NL"/>
+<TS version="2.1" language="nl_NL">
+    <context>
+        <name>behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+    </context>
+</TS>

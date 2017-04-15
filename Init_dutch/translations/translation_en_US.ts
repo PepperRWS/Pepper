f8dac5d0e8c93f924f4ace4eb7dda1259bf164f6
RobertRWS <Robert@192.168.2.11>
2017-04-16 00:05:35 +0200

    Initial commit, init basic channel Dutch for Pepper RB

---
+++ Init_dutch/translations/translation_en_US.ts
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>No tracking</source>
+            <comment>Text</comment>
+            <translation type="obsolete">No tracking</translation>
+        </message>
+        <message>
+            <source>Ready!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ready!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (2)</name>
+        <message>
+            <source>You are not looking at me!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You are not looking at me!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Nobody is here!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Nobody is here!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Nobody here?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Nobody here?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>You are not looking at me!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You are not looking at me!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Test/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Test/Say (1)</name>
+        <message>
+            <source>You are not looking at me!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You are not looking at me!</translation>
+        </message>
+    </context>
+</TS>
